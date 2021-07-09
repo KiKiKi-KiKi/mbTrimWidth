168fc90bc479f5d8e1609c146ee05b0de1ea126d
KiKiKi <chaika.design@gmail.com>
2021-07-09 18:13:56 +0900

    :construction: ESLint setting.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,22 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
-  extends: ['airbnb-base'],
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
   plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  rules: {},
+  rules: {
+    'newline-before-return': 'error',
+    'no-console': 'warn',
+    'no-var': 'error',
+  },
 };
